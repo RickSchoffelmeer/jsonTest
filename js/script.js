4b6f1b2f768d35fe27218e5d04ea6bceadbf14d4
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-05-19 15:37:38 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 async function getData() {
-
-    const requestURL = './json/markers.json';
-    const request = new Request(requestURL);
   
-    const response = await fetch(request);
+    const response = await fetch('./json/markers.json');
     const markers = await response.json();
 
     return markers
@@ -13,7 +10,7 @@ async function getData() {
 async function renderData() {
     let data = await getData()
     let el = document.getElementById('markers');
-    el.innerHTML = data.events.name;
+    el.innerHTML = data.events;
 }
 
 renderData();
