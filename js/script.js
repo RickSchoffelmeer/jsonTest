3d50383f5a7eb5d3ce515d5a258f1605dac5b334
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-05-19 15:02:22 +0200

    Update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+async function getData() {
+
+    const requestURL = './json/markers.json';
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const markers = await response.json();
+
+    return markers
+  
+  }
+
+async function renderData() {
+    let data = await getData()
+    let el = document.getElementById('data');
+    el.innerHTML = data.events.name;
+}
+
+renderData();
+
