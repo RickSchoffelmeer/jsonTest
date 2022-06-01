162422d0ab549b4a84c9646e2a0f3e628209d965
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-06-01 14:41:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 async function getData(){
-  const response = await fetch('./json/markers.json');
+  var response = await fetch('./json/markers.json');
   return response.json();
 }
 
 async function filterData(){
-  const data = await getData();
-  const result = data.filter(a => a.category == "Cultuur");
+  var data = await getData();
+  var result = data.filter(a => a.category == "Cultuur");
   console.log(result);
   document.querySelector("#markers").innerText = result
 }
