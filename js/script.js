17bee50939d0d4f8383f818bc30ff6bdd0d7aadd
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-06-01 12:16:07 +0200

    Update read JSON

--- js/script.js
+++ js/script.js
@@ -1,18 +1,2 @@
-async function getData() {
-  
-    await fetch("./json/markers.json")
-    .then(response => {
-      return response.json();
-    })
-    .then(jsondata => console.log(jsondata));
-  
-  }
-
-//async function renderData() {
-//    let data = await getData()
-//    let el = document.getElementById('markers');
-//    el.innerHTML = data.events;
-//}
-
-getData();
-
+const jsonData = require('./json/markers.json');
+console.log(jsonData);
