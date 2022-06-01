74c33a77395e6881d9fdb7ad4d42ae9eb7f665e0
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-06-01 13:49:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function GetData(){
   fetch('./json/markers.json')
     			.then(response => response.json())
     			.then(data => {
-            const result = data.filter(({category}) => category === "Cultuur")
+            const result = JSON.parse(data).filter(({category}) => category === "Cultuur")
 					  console.log(result)
       			document.querySelector("#markers").innerText = result
     			})
