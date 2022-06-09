90171644a2cbaac736e9a463675d5be4ac27ff49
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-06-09 13:57:28 +0200

    Big Update
    Extra Slide

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
 var queryItem;
+var data;
 
 async function getData(){
   const response = await fetch('./json/markers.json');
   return response.json();
 }
 
-async function filterData(catName){
-  const elements = document.getElementsByClassName('markers-box');
-  console.log(elements);
-  elements[0].style.display = 'block';
-  var data = await getData();
+async function filterDataCat(catName){
+  data = await getData();
   queryItem = catName;
   var result = data.events.filter(a => a.category === queryItem);
   console.log(result);
   document.querySelector("#markers").innerText = JSON.stringify(result);
+  data = result;
+}
+
+async function filterDataPers(persName){
+  const elements = document.getElementsByClassName('markers-box');
+  console.log(elements);
+  elements[0].style.display = 'block';
+  data = await getData();
+  queryItem = persName;
+  var result = data.events.filter(a => a.person === queryItem);
+  console.log(result);
+  document.querySelector("#markers").innerText = JSON.stringify(result);
+  data = result;
 }
 
 function clickFunction(elName, catName){
-  const elements = document.getElementsByClassName(elName);
-  console.log(elements);
-  elements[0].style.display = 'none';
-  filterData(catName);
+  if(elName == "side-one"){
+    const elements = document.getElementsByClassName(elName);
+    console.log(elements);
+    elements[0].style.display = 'none';
+    
+    const elementTwo = document.getElementsByClassName('slide-two');
+    console.log(elementTwo);
+    elementTwo[0].style.display = 'block';
+  }
+
+  if(elName == "side-two"){
+      const elements = document.getElementsByClassName(elName);
+      console.log(elements);
+      elements[0].style.display = 'none';
+    
+      filterDataPers(persName);
+  }
 }
