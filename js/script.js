c9d85a0f541818620310588e8c82c40f15ad22e2
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2022-06-09 12:27:44 +0200

    Big Update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
+var queryItem;
+
 async function getData(){
   const response = await fetch('./json/markers.json');
   return response.json();
 }
 
 async function filterData(){
-  const data = await getData();
-  const queryItem = "Cultuur"
-  const result = data.events.filter(a => a.category === queryItem);
+  var data = await getData();
+  queryItem = "Cultuur"
+  var result = data.events.filter(a => a.category === queryItem);
   console.log(result);
   document.querySelector("#markers").innerText = JSON.stringify(result);
 }
 
-filterData();
+async function clickFunction(element){
+  element.style.display = 'none';
+  document.querySelectorAll(".markers-box").style.display = 'inline-block';
+  filterData();
+}
